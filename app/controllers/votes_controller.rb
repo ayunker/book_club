ff54c056b89adde2fea77ecad2969bf69a27976d
Tony Yunker <tony.yunker@gmail.com>
2024-04-05 17:42:56 -0500

    feat: allow un-vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,5 @@
 class VotesController < ApplicationController
   def create
-    book = Book.find_by(book_params)
-
     if book
       Vote.find_or_create_by(
         book:,
@@ -13,7 +11,18 @@ class VotesController < ApplicationController
     redirect_back(fallback_location: meetings_url, notice: "voated!")
   end
 
+  def destroy
+    unless Vote.find(vote_id).destroy
+      redirect_back(fallback_location: meetings_url, notice: "fail!")
+    end
+    redirect_back(fallback_location: meetings_url, notice: "un-voated!")
+  end
+
   private
+
+  def book
+    Book.find_by(book_params)
+  end
 
   def session_id
     request.session_options.to_hash[:id].to_s
@@ -21,5 +30,11 @@ class VotesController < ApplicationController
 
   def book_params
     params.require(:book).permit(:id)
+  end
+
+  # TODO: something fishy with the way I'm handling this delete.
+  # TODO: i should really just use the form helper stuff?
+  def vote_id
+    params.require(:id)
   end
 end
